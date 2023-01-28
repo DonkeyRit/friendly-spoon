606c9fcff1fe13722ca6f1ec22be0ac5fefaa49e
Dima Alekseev <alekseevda24@gmail.com>
2023-01-28 16:43:02 +0100

    TEOTB-6: Update HttpClienExecutor

--- TelegramBotCore/src/main/java/com/github/donkeyrit/http/executor/HttpClientExecutor.java
+++ TelegramBotCore/src/main/java/com/github/donkeyrit/http/executor/HttpClientExecutor.java
@@ -1,12 +1,10 @@
 package com.github.donkeyrit.http.executor;
 
-import com.github.donkeyrit.exceptions.TelegramApiException;
-
 import java.io.IOException;
 import java.net.URI;
 
 public interface HttpClientExecutor<T, E>
 {
-    E Get(URI uri) throws TelegramApiException, IOException, InterruptedException;
-    E Post(URI uri, T content) throws TelegramApiException, IOException, InterruptedException;
+    E Get(URI uri) throws IOException, InterruptedException, Exception;
+    E Post(URI uri, T content) throws IOException, InterruptedException, Exception;
 }
