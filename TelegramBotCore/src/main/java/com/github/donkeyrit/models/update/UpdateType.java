2407ebfa4dab0f4588b7e5f541d5150efd2fb2c2
Dima Alekseev <alekseevda24@gmail.com>
2023-02-05 17:24:33 +0100

    TEOTB-6: Remove import

--- TelegramBotCore/src/main/java/com/github/donkeyrit/models/update/UpdateType.java
+++ TelegramBotCore/src/main/java/com/github/donkeyrit/models/update/UpdateType.java
@@ -1,6 +1,5 @@
 package com.github.donkeyrit.models.update;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonValue;
 
