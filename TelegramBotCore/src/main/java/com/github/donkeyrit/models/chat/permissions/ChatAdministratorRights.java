d9d9ac203369199ad1292586150476566d868137
Dima Alekseev <alekseevda24@gmail.com>
2023-02-05 16:36:15 +0100

    TEOTB-6: Add models for send message request

---
+++ TelegramBotCore/src/main/java/com/github/donkeyrit/models/chat/permissions/ChatAdministratorRights.java
@@ -0,0 +1,83 @@
+package com.github.donkeyrit.models.chat.permissions;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Optional;
+
+/**
+ * Represents the rights of an administrator in a chat.
+ * @see <a href="https://core.telegram.org/bots/api#chatadministratorrights">ChatAdministratorRights</a>
+ */
+public record ChatAdministratorRights(
+    /**
+     * True, if the user's presence in the chat is hidden
+     */
+    @JsonProperty(value = "is_anonymous")
+    boolean isAnonymous,
+    /**
+     * True, if the administrator can access 
+     *      - the chat event log, 
+     *      - chat statistics, 
+     *      - message statistics in channels, 
+     *      - see channel members, 
+     *      - see anonymous administrators in supergroups 
+     *      - and ignore slow mode.
+     * Implied by any other administrator privilege
+     */
+    @JsonProperty(value = "can_manage_chat")
+    boolean canManageChat,
+    /**
+     * True, if the administrator can delete messages of other users
+     */
+    @JsonProperty(value = "can_delete_messages")
+    boolean canDeleteMessages,
+    /**
+     * True, if the administrator can manage video chats
+     */
+    @JsonProperty(value = "can_manage_video_chats")
+    boolean canManageVideoChats,
+    /**
+     * True, if the administrator can restrict, ban or unban chat members
+     */
+    @JsonProperty(value = "can_restrict_members")
+    boolean canRestrictMembers,
+    /**
+     * True, if the administrator can add new administrators with a subset of their own privileges 
+     * or demote administrators that they have promoted, 
+     * directly or indirectly (promoted by administrators that were appointed by the user)
+     */
+    @JsonProperty(value = "can_promote_members")
+    boolean canPromoteMembers,
+    /**
+     * True, if the user is allowed to change the chat title, photo and other settings
+     */
+    @JsonProperty(value = "can_change_info")
+    boolean canChangeInfo,
+    /**
+     * True, if the user is allowed to invite new users to the chat
+     */
+    @JsonProperty(value = "can_invite_users")
+    boolean canInviteUsers,
+    /**
+     * True, if the administrator can post in the channel; channels only
+     */
+    @JsonProperty(value = "can_post_messages")
+    Optional<Boolean> canPostMessages,
+    /**
+     * True, if the administrator can edit messages of other users and can pin messages; channels only
+     */
+    @JsonProperty(value = "can_edit_messages")
+    Optional<Boolean> canEditMessages,
+    /**
+     * True, if the user is allowed to pin messages; groups and supergroups only
+     */
+    @JsonProperty(value = "can_pin_messages")
+    Optional<Boolean> canPinMessages,
+    /**
+     * True, if the user is allowed to create, rename, close, and reopen forum topics; supergroups only
+     */
+    @JsonProperty(value = "can_manage_topics")
+    Optional<Boolean> canManageTopics
+) 
+{
+    
+}
---
+++ TelegramBotCore/src/main/java/com/github/donkeyrit/models/keyboard/markup/ForceReply.java
@@ -0,0 +1,37 @@
+package com.github.donkeyrit.models.keyboard.markup;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Optional;
+
+/**
+ * Upon receiving a message with this object, 
+ * Telegram clients will display a reply interface to the user 
+ * (act as if the user has selected the bot's message and tapped 'Reply'). 
+ * This can be extremely useful if you want to create user-friendly step-by-step interfaces 
+ * without having to sacrifice privacy mode.
+ * @see <a href="https://core.telegram.org/bots/api#forcereply">ForceReply</a>
+ */
+public record ForceReply(
+    /**
+     * Shows reply interface to the user, as if they manually selected the bot's message and tapped 'Reply'
+     */
+    @JsonProperty(value = "force_reply")
+    boolean forceReply,
+    /**
+     * The placeholder to be shown in the input field when the reply is active; 1-64 characters
+     */
+    @JsonProperty(value = "input_field_placeholder")
+    Optional<String> inputFieldPlaceholder,
+    /**
+     * Use this parameter if you want to force reply from specific users only. 
+     * 
+     * Targets: 
+     *      1) users that are @mentioned in the text of the Message object; 
+     *      2) if the bot's message is a reply (has reply_to_message_id), sender of the original message.
+     */
+    Optional<Boolean> selective
+) 
+{
+    
+}
