75d3e9e42d9c26a4a7b0ddfa67516ffb0c7e3827
Dima Alekseev <alekseevda24@gmail.com>
2023-03-05 20:38:54 +0100

    TEOTB-9: Remove some unused code

--- TelegramBotCore/src/main/java/com/github/donkeyrit/App.java
+++ TelegramBotCore/src/main/java/com/github/donkeyrit/App.java
@@ -1,23 +1,10 @@
 package com.github.donkeyrit;
 
-import com.github.donkeyrit.configurations.models.TelegramBotConfigurationSettings;
-import com.github.donkeyrit.configurations.ConfigurationManager;
-import com.github.donkeyrit.http.executor.HttpClientExecutor;
-import com.github.donkeyrit.http.executor.HttpClientTelegramJsonExecutor;
-import com.github.donkeyrit.http.query.TelegramApiQueryBuilder;
 import com.github.donkeyrit.ioc.DaggerTelegramApiComponent;
 import com.github.donkeyrit.ioc.TelegramApiComponent;
 import com.github.donkeyrit.listeners.UpdateEventListener;
-import com.github.donkeyrit.http.query.QueryBuilder;
 import com.github.donkeyrit.models.response.User;
 import com.github.donkeyrit.bot.interfaces.TelegramBotFather;
-import com.github.donkeyrit.bot.TelegramBotFatherImpl;
-import com.github.donkeyrit.bot.interfaces.TelegramBot;
-import com.github.donkeyrit.bot.TelegramBotImpl;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.databind.JsonNode;
 
 public class App 
 {
@@ -29,21 +16,5 @@ public class App
     System.out.println("Bot - " + bot);
 
     botFather.registerUpdateEventListener(new UpdateEventListener());
-  }
-
-  private static void register()
-  {
-    TelegramBotConfigurationSettings configurationSettings = ConfigurationManager.GetTelegramBotConfiguration();
-    ObjectMapper jsonObjectMapper = new ObjectMapper();
-    jsonObjectMapper.registerModule(new Jdk8Module());
-    HttpClientExecutor<String, JsonNode> httpClientExecutor = new HttpClientTelegramJsonExecutor(jsonObjectMapper);
-    QueryBuilder queryBuilder = new TelegramApiQueryBuilder();
-    TelegramBot telegramPoolingBot = new TelegramBotImpl(
-        configurationSettings,
-        httpClientExecutor,
-        jsonObjectMapper,
-        queryBuilder);
-
-    TelegramBotFather botFather = new TelegramBotFatherImpl(telegramPoolingBot);
   }
 }
