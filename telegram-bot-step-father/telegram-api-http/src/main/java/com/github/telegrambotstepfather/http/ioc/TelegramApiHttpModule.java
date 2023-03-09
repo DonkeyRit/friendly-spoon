31fa33eab46723c0d5861da456d5724d3e577dfa
Dima Alekseev <alekseevda24@gmail.com>
2023-03-10 00:05:43 +0100

    TEOTB-13: Update ioc modules

--- telegram-bot-step-father/telegram-api-http/src/main/java/com/github/telegrambotstepfather/http/ioc/TelegramApiHttpModule.java
+++ telegram-bot-step-father/telegram-api-http/src/main/java/com/github/telegrambotstepfather/http/ioc/TelegramApiHttpModule.java
@@ -2,6 +2,8 @@ package com.github.telegrambotstepfather.http.ioc;
 
 import com.github.telegrambotstepfather.http.executor.HttpClientTelegramJsonExecutor;
 import com.github.telegrambotstepfather.http.executor.HttpClientExecutor;
+import com.github.telegrambotstepfather.http.query.TelegramApiQueryBuilder;
+import com.github.telegrambotstepfather.http.query.QueryBuilder;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.inject.AbstractModule;
 import com.google.inject.TypeLiteral;
@@ -12,6 +14,7 @@ public class TelegramApiHttpModule extends AbstractModule
     protected void configure() 
     {
         bind(createHttpClientExecutor()).to(HttpClientTelegramJsonExecutor.class);
+        bind(QueryBuilder.class).to(TelegramApiQueryBuilder.class);
     }
 
     private TypeLiteral<HttpClientExecutor<String, JsonNode>> createHttpClientExecutor()
