b57961c2f8fe8d7c38f739ba377ac87c300d4833
Dima Alekseev <alekseevda24@gmail.com>
2023-03-09 19:10:57 +0100

    TEOTB-13: Add new project for configurations

---
+++ telegram-bot-step-father/telegram-bot-step-father-configurations/src/main/java/com/github/telegrambotstepfather/configurations/ConfigurationManager.java
@@ -0,0 +1,35 @@
+package com.github.telegrambotstepfather.configurations;
+
+import com.github.telegrambotstepfather.configurations.models.TelegramBotConfigurationSettings;
+
+import java.io.FileNotFoundException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public final class ConfigurationManager 
+{
+    public static TelegramBotConfigurationSettings GetTelegramBotConfiguration()
+    {
+        try {
+            String configFilePath = ConfigurationFile.TELEGRAM_BOTS_CONFIGURATION_FILE.relativeFilePath;
+            Properties prop = GetProperties(configFilePath);
+            return new TelegramBotConfigurationSettings(prop.getProperty("BOT_TOKEN"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+
+    public static Properties GetProperties(String configFilePath) throws IOException
+    {
+        FileInputStream propsInput = new FileInputStream(configFilePath);
+        Properties prop = new Properties();
+        prop.load(propsInput);
+        return prop;   
+    }
+}
---
+++ telegram-bot-step-father/telegram-bot-step-father-configurations/src/main/java/com/github/telegrambotstepfather/configurations/models/TelegramBotConfigurationSettings.java
@@ -0,0 +1,7 @@
+package com.github.telegrambotstepfather.configurations.models;
+
+public record TelegramBotConfigurationSettings(
+    String botApiKey
+)
+{    
+}
