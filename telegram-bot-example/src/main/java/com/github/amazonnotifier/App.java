fc644559255d234577a46b19cddad8ec23d43e27
Dima Alekseev <alekseevda24@gmail.com>
2023-03-10 09:38:54 +0100

    TEOTB-13: Make buildable bot example

--- telegram-bot-example/src/main/java/com/github/amazonnotifier/App.java
+++ telegram-bot-example/src/main/java/com/github/amazonnotifier/App.java
@@ -1,34 +1,34 @@
 package com.github.amazonnotifier;
 
-import com.github.donkeyrit.bot.interfaces.TelegramBot;
-import com.github.donkeyrit.bot.interfaces.TelegramBotFather;
-import com.github.donkeyrit.ioc.TelegramApiBaseModules;
-import com.github.donkeyrit.listeners.UpdateEventListener;
-import com.github.donkeyrit.models.response.User;
+import com.github.amazonnotifier.site.AmazonScrapper;
 
 import com.google.inject.Injector;
 import com.google.inject.Guice;
+
+import java.io.IOException;
 import java.util.logging.Logger;
 
 public class App 
 {
-
-    public static void main(String[] args)
+    
+    public static void main(String[] args) throws IOException
     {
         String url = "https://www.amazon.com/gp/product/B0BBHD5D8Y/ref=ox_sc_saved_title_1?smid=ATVPDKIKX0DER&psc=1";
 
+        AmazonScrapper scrapper = new AmazonScrapper();
+        scrapper.parse(url);
 
         
-        Injector injector = Guice.createInjector(new TelegramApiBaseModules());
+        // Injector injector = Guice.createInjector(new TelegramApiBaseModules());
     
-        Logger logger = injector.getInstance(Logger.class);
-        logger.info("Start application...");
+        // Logger logger = injector.getInstance(Logger.class);
+        // logger.info("Start application...");
     
-        TelegramBotFather botFather = injector.getInstance(TelegramBotFather.class);
-        User bot = botFather.init();
-        logger.info(() -> "Telegram Bot - " + bot.firstName());
+        // TelegramBotFather botFather = injector.getInstance(TelegramBotFather.class);
+        // User bot = botFather.init();
+        // logger.info(() -> "Telegram Bot - " + bot.firstName());
     
-        UpdateEventListener eventListener = new UpdateEventListener(logger, injector.getInstance(TelegramBot.class));
-        botFather.getUpdatesEventSource().addListener(eventListener);
+        // UpdateEventListener eventListener = new UpdateEventListener(logger, injector.getInstance(TelegramBot.class));
+        // botFather.getUpdatesEventSource().addListener(eventListener);
     }
 }
--- telegram-bot-example/src/main/java/com/github/amazonnotifier/site/AmazonScrapper.java
+++ telegram-bot-example/src/main/java/com/github/amazonnotifier/site/AmazonScrapper.java
@@ -1,18 +1,27 @@
 package com.github.amazonnotifier.site;
 
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import org.jsoup.Jsoup;
+
 public class AmazonScrapper 
 {
 
-    public void parse(String url)
+    public void parse(String url) throws IOException
     {
         Document doc = Jsoup
             .connect(url)
             .get();
         
-        Element deliveryMessage = doc.selectFirst("#ddmDeliveryMessage > b");
-        String deliveryDate = deliveryMessage
-            .text()
-            .trim();
-        System.out.println(deliveryDate);
+        Element deliveryMessage = doc.selectFirst("#mir-layout-DELIVERY_BLOCK");
+        if(deliveryMessage != null)
+        {
+            String deliveryDate = deliveryMessage
+                .text()
+                .trim();
+            System.out.println(deliveryDate);
+        }
     }
 }
