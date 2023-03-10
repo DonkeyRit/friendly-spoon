738f36c276e54355254779e3fa9dd1988344f231
Dima Alekseev <alekseevda24@gmail.com>
2023-03-10 10:31:11 +0100

    TEOTB-13: Extract delivery date for current location

--- telegram-bot-example/src/main/java/com/github/amazonnotifier/site/AmazonScrapper.java
+++ telegram-bot-example/src/main/java/com/github/amazonnotifier/site/AmazonScrapper.java
@@ -4,6 +4,8 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 
 public class AmazonScrapper 
@@ -11,11 +13,22 @@ public class AmazonScrapper
 
     public void parse(String url) throws IOException
     {
-        Document doc = Jsoup
-            .connect(url)
-            .get();
+        // Set up the request headers and cookies
+        Connection connection = Jsoup.connect(url)
+            .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
+            .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+            .header("Accept-Language", "en-US,en;q=0.5")
+            .header("Referer", "https://www.google.com/")
+            .header("Connection", "keep-alive")
+            .cookie("name", "value")
+            .cookie("another_name", "another_value");
+
+        // Send the request and retrieve the response
+        Document doc = connection.get();
         
-        Element deliveryMessage = doc.selectFirst("#mir-layout-DELIVERY_BLOCK");
+        System.out.println(doc.outerHtml());
+
+        Element deliveryMessage = doc.selectFirst("#deliveryBlockMessage");
         if(deliveryMessage != null)
         {
             String deliveryDate = deliveryMessage
