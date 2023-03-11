fd27f8c0e7ca0fd69d9fbba7a74ff80f3c9b019f
Dima Alekseev <alekseevda24@gmail.com>
2023-03-11 19:07:22 +0100

    TEOTB-13: Update configuration file path

--- telegram-bot-step-father/telegram-bot-step-father-configurations/src/main/java/com/github/telegrambotstepfather/configurations/ConfigurationFile.java
+++ telegram-bot-step-father/telegram-bot-step-father-configurations/src/main/java/com/github/telegrambotstepfather/configurations/ConfigurationFile.java
@@ -2,7 +2,7 @@ package com.github.telegrambotstepfather.configurations;
 
 public enum ConfigurationFile 
 {
-    TELEGRAM_BOTS_CONFIGURATION_FILE("TelegramBotCore/src/resources/config.properties");
+    TELEGRAM_BOTS_CONFIGURATION_FILE("resources/config.properties");
 
     public final String relativeFilePath;
 
